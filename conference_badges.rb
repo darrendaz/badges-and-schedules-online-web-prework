1c4de62b742a896d839fc17a3e88ee195c56ced0
Darren L <darren.lou@gmail.com>
2019-02-20 07:17:22 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,11 +4,9 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(arrayOfNames)
-  badgeArray = arrayOfNames.map {|name|
+  arrayOfNames.map {|name|
     badge_maker(name)
   }
-  
-  return badgeArray
 end
 
 def assign_rooms(speakers)
@@ -17,7 +15,6 @@ def assign_rooms(speakers)
    speakers.each_with_index { |speaker, index|
     rooms[index] = "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"
   }
-  
  return rooms
   
 
